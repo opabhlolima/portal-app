da8a98bec5038cc1ba504f14a46b906ac0d4e5c9
jrvieirapr <105877022+jrvieirapr@users.noreply.github.com>
2024-10-02 23:44:09 +0000

    Model Autor

--- app/Models/Autor.php
+++ app/Models/Autor.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Autor extends Model
 {
     use HasFactory;
+
+    //Criação em massa e evita de deixa colocar 
+    //dados diferentes no banco
+    protected $fillable = ['nome', 'contato'];
+
+    //quando eu alterei o nome do meu banco no migrations
+    protected $table = 'autores';
+
+    // explorar relacionametno com o noticias
+    public function noticias(){
+        return $this->hasMany(Noticia::class);
+    }
+
+    // Relacionamentos
+    //Pai
+    //Filhos
 }
