31a5f9419854da35d35805e1724c00137d340fff
jrvieirapr <105877022+jrvieirapr@users.noreply.github.com>
2024-10-10 22:47:58 +0000

    layout app

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Senges - @yield('title')</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Navegacao -->
+    <header>
+        <!-- inserir o menu de navegação -->
+        @include('partials.navbar')
+    </header>
+    <!-- Mostrar o conteudo principal -->
+     <main>
+        <div class="container">
+            @yield('conteudo')
+        </div>
+     </main>
+     <!-- Rodape: links contato mapas e outros -->
+    <footer>
+        @include('partials.rodape')
+    </footer>
+    
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
